c1f287c8393256ffa7cc077f194cd2394a08fe2c
Simran Parmar <simranparmar3220034@gmail.com>
2023-05-17 20:57:53 +0530

    Retrieved Firebase Data into Recyclerview.

--- app/src/main/java/com/example/messenger/MainActivity.java
+++ app/src/main/java/com/example/messenger/MainActivity.java
@@ -1,22 +1,72 @@
 package com.example.messenger;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.ktx.Firebase;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     FirebaseAuth auth;
+    RecyclerView mainUserRecyclerview;
+    UserAdapter adapter;
+    FirebaseDatabase database;
+    ArrayList<Users> userArrayList;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+
+        database = FirebaseDatabase.getInstance();
         auth = FirebaseAuth.getInstance();
+
+        DatabaseReference refrence = database.getReference().child("user");
+
+        userArrayList = new ArrayList<Users>();
+
+        refrence.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot dataSnapshot: snapshot.getChildren())
+                {
+                    Users users = dataSnapshot.getValue(Users.class);
+                    userArrayList.add(users);
+                }
+                adapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+        mainUserRecyclerview = findViewById(R.id.mainUserRecyclerview);
+        mainUserRecyclerview.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new UserAdapter(MainActivity.this,userArrayList);
+        mainUserRecyclerview.setAdapter(adapter);
+
+
         if(auth.getCurrentUser() == null){
            Intent intent=new Intent(MainActivity.this,login.class);
             startActivity(intent);
